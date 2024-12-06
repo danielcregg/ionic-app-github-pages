8675dc9965f2e6c40a9577778bb79b1a374c09c8
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-12-06 21:58:31 +0000

    Refactor app structure: replace home page with tabs layout and add explore container component

--- my-ionic-app/src/main.ts
+++ my-ionic-app/src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { provideServiceWorker } from '@angular/service-worker';
-import { environment } from './environments/environment';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
@@ -12,6 +10,5 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
-    provideServiceWorker('ngsw-worker.js', { enabled: environment.production }),
   ],
 });
